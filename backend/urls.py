b294b3ff4ac138272add1d67714ebf29f8454651
Dishant Yadav <dishantyadav599@gmail.com>
2024-02-28 01:42:49 +0530

    added render.yaml file

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("snippify.urls")),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
