3ace192b4a9be9993982a835bc078ab1a83310fb
Dishant Yadav <dishantyadav599@gmail.com>
2024-02-28 03:01:58 +0530

    updated setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,4 +5,4 @@ python3 manage.py migrate
 # python3 manage.py makemigrations snippify
 # python3 manage.py migrate snippify
 python3 manage.py migrate --run-syncdb
-source '/etc/secrets/.env.admin' && python manage.py createsuperuser --name=admin --email=admin@mail1.com --is_admin=True --noinput
+source '.env' && python manage.py createsuperuser --name=admin --email=admin@mail.com --is_admin=True --noinput
