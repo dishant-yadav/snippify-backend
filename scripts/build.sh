1f389e966e4eba271056e3947919e4bcb1735c0e
Dishant Yadav <dishantyadav599@gmail.com>
2024-02-28 02:08:23 +0530

    updated the build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,8 @@ pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
+python3 manage.py makemigrations snippify
+python3 manage.py migrate snippify
+python3 manage.py migrate --run-syncdb
+
 source '.env' && python manage.py createsuperuser --name=admin --email=admin@mail.com --noinput
