ea130304907de0a5a6b42ed8ee0629e4811da6c0
Dishant Yadav <dishantyadav599@gmail.com>
2024-02-28 02:30:04 +0530

    changes in setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,6 @@ python3.8 -m pip install --upgrade pip
 pip install -r requirements.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
-python3 manage.py makemigrations snippify
-python3 manage.py migrate snippify
+# python3 manage.py makemigrations snippify
+# python3 manage.py migrate snippify
 python3 manage.py migrate --run-syncdb
