57581ba2b7d7416f8640cd9299543c1acc0d28d0
Dishant Yadav <dishantyadav599@gmail.com>
2024-03-08 00:00:26 +0530

    modified the build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,8 @@ python manage.py collectstatic --no-input
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-python3 manage.py makemigrations snippify
-python3 manage.py migrate snippify
+# python3 manage.py makemigrations snippify
+# python3 manage.py migrate snippify
 python3 manage.py migrate --run-syncdb
 
 python manage.py createsuperuser --name=admin --email=admin@mail.com --noinput
