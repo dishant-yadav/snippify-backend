501936b5ee794832e4227fde83f76f2ea855d810
Dishant Yadav <dishantyadav599@gmail.com>
2024-03-07 23:53:14 +0530

    updated the scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,7 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 
-# python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
 python3 manage.py makemigrations
 python3 manage.py migrate
@@ -13,4 +10,4 @@ python3 manage.py makemigrations snippify
 python3 manage.py migrate snippify
 python3 manage.py migrate --run-syncdb
 
-source '.env' && python manage.py createsuperuser --name=admin --email=admin@mail.com --noinput
+python manage.py createsuperuser --name=admin --email=admin@mail.com --noinput
