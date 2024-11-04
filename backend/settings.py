a3abb9555139c85f0198b6976a7ffbbd9b59254c
Dishant Yadav <97543666+dishant-yadav@users.noreply.github.com>
2024-11-04 12:58:29 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -201,5 +201,5 @@ DJOSER = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://snippify-code.vercel.app/",
+    "https://snippify-code.vercel.app",
 ]
